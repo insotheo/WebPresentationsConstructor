5cc5b65411c3853b2e2cb0e60abca5a99b534703
insotheo <maxim.insotheo@gmail.com>
2023-12-09 14:22:52 +0400

    Добавьте файлы проекта.

---
+++ EnterMenu/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Windows;
+using System;
+using MessageBoxesWindows;
+
+namespace ProjectsManager
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            try
+            {
+                string appDir = Directory.GetCurrentDirectory();
+
+                if (!Directory.Exists(Path.Combine(appDir, "projects")))
+                    Directory.CreateDirectory(Path.Combine(appDir, "projects"));
+
+                if (!Directory.Exists(Path.Combine(appDir, "styles")) || !Directory.Exists(Path.Combine(appDir, "scripts")))
+                    throw new Exception("Нарушена целостность приложения!");
+
+            }
+            catch (Exception ex)
+            {
+                MessBox.showError(ex.Message);
+                Environment.Exit(-1);
+            }
+
+            InitializeComponent();
+            GC.Collect();
+        }
+    }
+}
---
+++ MessageBoxesWindows/MessBox.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace MessageBoxesWindows
+{
+    public class MessBox
+    {
+        public static void showError(string message)
+        {
+            MessageBox.Show(message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}

7cf1f9bc6db9a5994e397c3b80e1e14226204d9f
insotheo <maxim.insotheo@gmail.com>
2023-12-28 19:42:15 +0400

    resources updated

--- EngineResources/scripts/baseScript.js
+++ EngineResources/scripts/baseScript.js
@@ -3,3 +3,15 @@ function say(message){
     console.log("WPC_Editor::User: " + message);
     alert(message);
 }
+
+//_EDITOR::FUNCTIONS::TYPES::EVENT
+function quit(){
+    window.close();
+}
+
+//_EDITOR::FUNCTIONS::TYPES::EVENT
+function askThenQuit(){
+    if(confirm("Вы уверены, что хотите закрыть страницу?") == true){
+        window.close();
+    }
+}

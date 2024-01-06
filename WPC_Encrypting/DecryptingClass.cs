7c72400911b8a92a1c42ce8b484008ec8384ea86
insotheo <maxim.insotheo@gmail.com>
2024-01-06 16:44:33 +0400

    WPC_Encrypting.dll

---
+++ WPC_Encrypting/DecryptingClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace WPC_Encrypting
+{
+    public class DecryptingClass
+    {
+        public static string decryptString(string text)
+        {
+            if(text != String.Empty)
+            {
+                byte[] encryptedBytes = Convert.FromBase64String(text);
+                byte[] decryptedBytes = new byte[encryptedBytes.Length];
+                for(int i = 0; i < encryptedBytes.Length; i++)
+                {
+                    decryptedBytes[i] = (byte)(encryptedBytes[i] - 1);
+                }
+                return Encoding.UTF8.GetString(decryptedBytes);
+            }
+            else
+            {
+                return text;
+            }
+        }
+
+        public static string[] encryptString(string[] text)
+        {
+            if(text.Length != 0)
+            {
+                for(int i = 0; i < text.Length; i++)
+                {
+                    decryptString(text[i]);
+                }
+                return text;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ WPC_Encrypting/EncryptingClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace WPC_Encrypting
+{
+    public class EncryptingClass
+    {
+        public static string encryptString(string input)
+        {
+            if (input != String.Empty)
+            {
+                byte[] plainBytes = Encoding.UTF8.GetBytes(input);
+                byte[] encryptedBytes = new byte[plainBytes.Length];
+                for (int i = 0; i < plainBytes.Length; i++)
+                {
+                    encryptedBytes[i] = (byte)(plainBytes[i] + 1);
+                }
+                return Convert.ToBase64String(encryptedBytes);
+            }
+            else
+            {
+                return input;
+            }
+        }
+
+        public static string[] encryptString(string[] input)
+        {
+            if(input.Length != 0)
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    encryptString(input[i]);
+                }
+                return input;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}

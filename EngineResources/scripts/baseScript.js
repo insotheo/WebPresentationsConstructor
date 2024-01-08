f5da0c34d27390295b07ebcc7346635325b4b342
insotheo <maxim.insotheo@gmail.com>
2024-01-08 12:08:14 +0400

    JS source updated

--- EngineResources/scripts/baseScript.js
+++ EngineResources/scripts/baseScript.js
@@ -16,3 +16,78 @@ function askThenQuit(){
         window.close();
     }
 }
+
+//_EDITOR::FUNCTIONS::TYPES::EVENT
+function goPage(page_name){
+    if(page_name != null){
+        let str_page_name = toString(page_name);
+        let doesPageExist = false;
+        if(!str_page_name.endsWith(".html")){
+        str_page_name += ".html";  
+        } 
+        let http = new XMLHttpRequest();
+        http.open("HEAD", str_page_name, false);
+        http.send();
+        if(http.status != 404){
+            doesPageExist = true;
+        }
+        if(doesPageExist){
+            window.open(str_page_name, "_blank");
+        }
+        else{
+            alert("Ошибка открытия страницы!");
+            console.log("ERROR: page to open(${str_page_name}) doesn't exist![WPC]");
+        }
+    }
+    else{
+        console.log("ERROR: null ref exeption![WPC]");
+    }
+}
+
+//_EDITOR::FUNCTIONS::TYPES::EVENT
+function reloadPage(){
+    location.reload();
+}
+
+//_EDITOR::FUNCTIONS::TYPES::EVENT
+function replaceText(element_name, new_text){
+    if(element_name != null){
+        let el = document.getElementById(element_name);
+        if(el != null){
+            el.textContent = new_text;
+            console.log("DONE![WPC]");
+        }
+        else{
+            console.log("ERROR: null ref exeption![WPC]");
+        }
+    }
+}
+
+function replaceText(element_name, old_text_part, new_text_part){
+    if(element_name != null){
+        let el = document.getElementById(element_name);
+        if(el != null){
+            el.textContent = el.textContent.replace(old_text_part, new_text_part);
+            console.log("DONE![WPC]");
+        }
+        else{
+            console.log("ERROR: null ref exeption![WPC]");
+        }
+    }
+}
+
+//_EDITOR::FUNCTIONS::TYPES::EVENT
+function removeElement(element_name){
+    if(element_name != null){
+        let elID = document.getElementById(element_name);
+        if(elID != null){
+            elID.remove();
+        }
+        else{
+            console.log("ERROR: null ref exeption![WPC]");
+        }
+    }
+    else{
+        console.log("ERROR: null ref exeption![WPC]");
+    }
+}
